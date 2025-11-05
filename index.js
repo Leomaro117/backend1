9b7052a5352e5ea11d71460cf9c61f9dc76f7c55
Leomaro117 <leomr0432@gmail.com>
2025-11-05 23:17:39 +0000

    actualizacion

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 const express = require("express")
 const app = express()
-app.use(express.json())
+app.use(express.json());
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 
-const requesLogger = (request, response, next) => {
+const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method);
     console.log('Path: ', request.path);
     console.log('Body: ', request.body);
-    console.log('------------------------------------');
+    console.log('----------------------');
     next()
 }
 
-app.use(requesLogger)
+app.use(requestLogger)
 
 let notes = [
     {
         id: 1,
-        content: 'HTML is easy',
+        content: "HTML is easy",
         important: true
     },
-
     {
         id: 2,
-        content: 'Browser can execute only JavaScript',
-        important: true
+        content: "Browser can execute only JavaScript",
+        important: false
     },
-
     {
         id: 3,
-        content: 'GET & POST are the most important methods of HTTP Protocols',
+        content: "GET & POST are the most importants methods of HTTP Protocols",
         important: true
     }
 ]
-
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
@@ -43,47 +40,45 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(x => x.id === id)
-
-    if (note) {
+    if(note)
+    {
         response.json(note)
     }
-
-    else {
+    else
+    {
         response.status(404).end()
     }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(x => x.id !== id)  
+    notes = notes.filter(x => x.id !== id)
     response.status(204).end()
-    console.log('Delete ', id)
 })
 
 app.post('/api/notes', (request, response) => {
     const note = request.body
-    if (note.content){
-        const note = request.body
-        console.log(note)
-        note.id = notes.length+1
+    if(note.content)
+    {
+        note.id = notes.length + 1
         notes = notes.concat(note)
-        response.json(note)
-        console.log('Adding: ' , note);
-    }else{
+        response.json(note);
+    }
+    else 
+    {
         response.status(400).json({error: 'content is missing'})
     }
-    
-    
 })
 
-const badPath= (request, response, next) => {
-    response.status(404).send({ error: 'Ruta desconocida'})
+const badPath = (request, response, next) => {
+    response.status(404).send({error: 'Ruta desconocida'})
 }
 
 app.use(badPath)
 
-const PORT = 3001
+//Para que escoja el puerto
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 })
